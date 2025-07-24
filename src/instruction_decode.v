e91106dd4a93ea30146970af0a174549a1db6813
sanjaycal <sanjay.calgary@gmail.com>
2025-07-23 22:13:30 -0400

    WHY IS SPICE CASE INSENSITIVE?????

--- src/instruction_decode.v
+++ src/instruction_decode.v
@@ -15,7 +15,7 @@ module instruction_decode (
     input  wire       nmi,
     input  wire       rdy,
     input  wire [6:0] processor_status_register_read,
-    output reg [6:0] processor_status_register_write,        
+    output reg [6:0] processor_status_register_write,
     output reg [15:0] memory_address,  // better name for this
     output reg [1:0]    address_select, // 0 = PC, 1 = Memory Address (Remove?), 2 = ALU,
     output reg       processor_status_register_rw,
@@ -24,7 +24,7 @@ module instruction_decode (
     output reg [1:0] input_data_latch_enable, // 00 IDLE, 01 LOAD, 10 STORE
     output reg       pc_enable,
     output reg [4:0] alu_enable,
-    output reg [2:0] accumulator_enable, // BIT 2 is enable, BIT 1 is R/W_n and BIT 0 is BUS SELECT         
+    output reg [2:0] accumulator_enable, // BIT 2 is enable, BIT 1 is R/W_n and BIT 0 is BUS SELECT
     output reg [2:0] stack_pointer_register_enable, // 0 is light blue and 1 is dark blue.
     output reg [2:0] index_register_X_enable,
     output reg [2:0] index_register_Y_enable
@@ -35,7 +35,7 @@ localparam S_IDLE           = 4'd0;
 localparam S_OPCODE_READ    = 4'd1;
 localparam S_ZPG_ABS_ADR_READ   = 4'd2;
 localparam S_IDL_DATA_WRITE = 4'd3;
-localparam S_IDL_ADR_WRITE  = 4'd4; 
+localparam S_IDL_ADR_WRITE  = 4'd4;
 localparam S_ALU_FINAL      = 4'd5; // Final implies that there isn't anymore branching between this state and OPCODE_READ
 localparam S_DBUF_OUTPUT    = 4'd6;
 localparam S_ALU_TMX        = 4'd7;
@@ -48,7 +48,7 @@ localparam S_ABS_HB         = 4'd11;
 
 reg [3:0] STATE      = S_IDLE;
 reg [3:0] NEXT_STATE = S_IDLE;
-reg [15:0] MEMORY_ADDRESS_INTERNAL  = 16'b0; 
+reg [15:0] MEMORY_ADDRESS_INTERNAL  = 16'b0;
 reg [2:0] ADDRESSING;
 reg [7:0] OPCODE;
 reg [7:0] INSTRUCTION;
@@ -61,9 +61,9 @@ reg [7:0] INSTRUCTION;
 
     //assign is_shift_rotate_op = (OPCODE[1:0] == 2'b10);
     //assign addr_mode_bits = OPCODE[4:2];
-    //assign is_target_addr_mode = ((addr_mode_bits == `ADR_ZPG) ||   
-    //                            (addr_mode_bits == `ADR_ABS)  || 
-    //                            (addr_mode_bits == `ADR_ZPG_X)); 
+    //assign is_target_addr_mode = ((addr_mode_bits == `ADR_ZPG) ||
+    //                            (addr_mode_bits == `ADR_ABS)  ||
+    //                            (addr_mode_bits == `ADR_ZPG_X));
 
 always @(*) begin
     NEXT_STATE = STATE;
@@ -86,7 +86,7 @@ always @(*) begin
             NEXT_STATE = S_ALU_FINAL;   // because this involves registers we can go straight to final
         end else begin
             NEXT_STATE = S_IDLE; // Default case, should not happen.
-        end  
+        end
     end
     S_ZPG_ABS_ADR_READ: begin
         NEXT_STATE = S_IDL_DATA_WRITE;
@@ -128,7 +128,7 @@ always @(*) begin
         end else if(ADDRESSING == `ADR_A) begin
             NEXT_STATE = S_OPCODE_READ;
         end
-    end 
+    end
     S_DBUF_OUTPUT: begin
         NEXT_STATE = S_IDLE;
     end
@@ -171,7 +171,7 @@ always @(posedge clk ) begin
         index_register_X_enable <= `BUF_IDLE_THREE;
         index_register_Y_enable <= `BUF_IDLE_THREE;
         if(NEXT_STATE == S_OPCODE_READ) begin
-            pc_enable <= 1; 
+            pc_enable <= 1;
             OPCODE <= instruction;
             if(instruction[4:2] == `ADR_ZPG) begin
                 ADDRESSING <= `ADR_ZPG;
@@ -242,7 +242,7 @@ always @(posedge clk ) begin
                 input_data_latch_enable <= `BUF_STORE_TWO;
                 alu_enable <= `INC;
                 processor_status_register_write <= `ZERO_FLAG | `NEGATIVE_FLAG;
-            end 
+            end
             // LOAD
             else if(OPCODE == `OP_LD_X_ZPG || OPCODE==`OP_LD_A_ZPG || OPCODE==`OP_LD_Y_ZPG) begin
                 input_data_latch_enable <= `BUF_STORE_TWO;
@@ -281,7 +281,7 @@ always @(posedge clk ) begin
             else if(ADDRESSING == `ADR_ZPG || ADDRESSING == `ADR_ZPG_X || ADDRESSING == `ADR_ABS) begin
                 data_buffer_enable <= `BUF_LOAD_TWO;
                 alu_enable <= `TMX;
-            end 
+            end
 	    else if(ADDRESSING == `ADR_A) begin
                 accumulator_enable <= `BUF_LOAD2_THREE;
                 alu_enable <= `TMX;
